bd27711c16d189626c6a20d1d34c923e11d625f2
Vishnu Vardhan Dhandu <vdhandu@modeln.com>
2021-10-06 14:59:47 +0530

    ample

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// vishnu this is additional
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
